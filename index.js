17919a6d062f906410a0c9b39be7c361090f3fb3
horkworse <horkworse@gmail.com>
2021-11-18 04:23:13 +0500

    add mongoose connect, add dish and dishType routers

--- index.js
+++ index.js
@@ -15,23 +15,27 @@ const orderSchema = require("./Schemes/orderSchema");
 const cardSchema = require("./Schemes/cardSchema");
 const distributionPointSchema = require("./Schemes/distributionPointSchema");
 const orderStatusSchema = require("./Schemes/orderStatusSchema");
+const {ObjectId} = require("mongodb");
 
-// const  ingredientModel = mongoose.model(" ingredient",  ingredientSchema);
-const  dishTypeModel = mongoose.model(" dishType",  dishTypeSchema);
-const  dishModel = mongoose.model(" dish",  dishSchema);
-// const  orderedDishModel = mongoose.model(" orderedDish",  orderedDishSchema);
-// const  userModel = mongoose.model(" user",  userSchema);
-// const  orderModel = mongoose.model(" order",  orderSchema);
-// const  cardModel = mongoose.model(" card",  cardSchema);
-// const  distributionPointModel = mongoose.model(" distributionPoint",  distributionPointSchema);
-// const  orderStatusModel = mongoose.model(" orderStatus",  orderStatusSchema);
+const Ingredient = mongoose.model(" ingredient", ingredientSchema);
+const DishType = mongoose.model(" dishType", dishTypeSchema);
+const Dish = mongoose.model(" dish", dishSchema);
+const OrderedDish = mongoose.model(" orderedDish", orderedDishSchema);
+const User = mongoose.model(" user", userSchema);
+const Order = mongoose.model(" order", orderSchema);
+const Card = mongoose.model(" card", cardSchema);
+const DistributionPoint = mongoose.model(" distributionPoint", distributionPointSchema);
+const OrderStatus = mongoose.model(" orderStatus", orderStatusSchema);
 
-mongoose.connect("mongodb+srv://horkworse:rtf-123@cluster0.phklg.mongodb.net/eda-backend?retryWrites=true&w=majority", function(err){
-    if(err) return console.log(err);
-    app.listen(3000, function(){
+const connectionString = `mongodb+srv://${config.get("dbuser")}:${config.get("dbpassword")}@cluster0.phklg.mongodb.net/${config.get("dbname")}?retryWrites=true&w=majority`;
+mongoose.connect(connectionString, function (err) {
+    if (err) console.log(err);
+    app.listen(3000, function () {
         console.log("Сервер ожидает подключения...");
     });
 });
+
+const userRouter = express.Router();
 
 app.use((req, res, next) => {
     log.info("request ", req.url);
@@ -43,117 +47,197 @@ app.use((req, res, next) => {
     return next();
 });
 
+const dishRouter = express.Router();
+
+dishRouter.get("/", (req, res) => {
+    Dish.find({}, (err, dishes) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+        res.send(dishes);
+    });
+});
+
+dishRouter.get("/:id", (req, res) => {
+    Dish.findOne({_id: ObjectId(req.params.id)}, (err) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+        res.send(dish);
+    });
+});
+
+dishRouter.put("/edit", (req, res) => {
+    let newDish = {
+        name: req.query.name,
+        description: req.query.description,
+        price: req.query.price,
+        image: req.query.image,
+        ingredient: req.query.ingredient,
+        type: req.query.type
+    };
+
+    Dish.findOneAndUpdate({_id: req.params.id}, newDish, (err) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+        res.send(200);
+    });
+});
+
+dishRouter.post("/add", (req, res) => {
+    let dish = new Dish({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        image: req.body.image,
+        ingredient: req.body.ingredient,
+        type: req.body.type
+    });
+
+    dish.save((err, dishes) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+        res.sendStatus(200);
+    });
+});
+
+dishRouter.delete("/delete/:id", (req, res) => {
+    Dish.findByIdAndDelete(ObjectId(req.params.id), (err) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+        res.sendStatus(200);
+    });
+});
+
+app.use("/dish", dishRouter);
+
+const dishTypeRouter = express.Router();
+
+dishTypeRouter.get("/", (req, res) => {
+    DishType.find({}, (err, dishTypes) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+        res.send(dishTypes);
+    });
+});
+
+dishTypeRouter.get("/:id", (req, res) => {
+    DishType.findOne({_id: ObjectId(req.params.id)}, (err, dishType) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+        res.send(dishType);
+    })
+});
+
+dishTypeRouter.put("/edit", (req, res) => {
+    let newDishType = {
+        name: req.query.name
+    };
+
+    DishType.findOneAndUpdate({_id: req.params.id}, newDishType, (err) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+        res.send(200);
+    })
+});
+
+dishTypeRouter.post("/add", (req, res) => {
+    let dishType = new DishType({
+        name: req.body.name
+    });
+
+    dishType.save((err) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+        res.sendStatus(200);
+    });
+});
+
+dishTypeRouter.delete("/delete/:id", (req, res) => {
+    DishType.findByIdAndDelete(ObjectId(req.params.id), (err) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+        res.sendStatus(200);
+    });
+});
+
+app.use("/dishType", dishTypeRouter);
+
+
+
+
+
 app.get("/user/", (req, res) => {
 
 });
 
 app.get("/", (req, res) => {
-    res.send("<h2>Привет Express!</h2>");
+    res.send("<h2>Ну привет, пидарюга (:</h2>");
 });
+
+const ingredientRouter = express.Router();
 
 app.get("/ingredient", (req, res) => {
-    ingredientModel.find({}, function(err, ingredients){
-        if(err) return console.log(err);
-        res.send(ingredients)
+    Ingredient.find({}, (err, ingredients) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+        res.send(ingredients);
     });
 });
 
-app.get("/dishType", (req, res) => {
-    dishTypeModel.find({}, function(err, types){
-        if(err) return console.log(err);
-        res.send(types)
-    });
-});
-
-app.get("/dish", (req, res) => {
-    dishModel.find({}, function(err, dishes){
-        if(err) return console.log(err);
-        res.send(dishes)
-    });
-});
-
-app.get("/dish/insert", (req, res) => {
-    // if(!req.body) return res.sendStatus(400);
-
-    const coffee = {
-        name: "Капучино",
-        description: "Ароматный свежий капучино",
-        price: 45,
-        image: ["https://coffeemania.ru/uploads/p16/5b3f45a1794a064.jpg"],
-        ingredient: [{
-            name: "Зерна кофе",
-            price: 0,
-            required: true,
-            additional: false
-        }, {
-            name: "Молоко",
-            price: 0,
-            required: true,
-            additional: false
-        }],
-        type: ""
-    };
-
-    let salad = {
-        name: "Салат весна",
-        description: "Ароматный свежий салат",
-        price: 45,
-        image: ["https://static.1000.menu/img/content/37230/salat-graciya_1564753184_1_max.jpg"],
-        ingredient: [{
-            name: "Капуста",
-            price: 0,
-            required: true,
-            additional: false
-        }, {
-            name: "Перец",
-            price: 0,
-            required: true,
-            additional: false
-        }, {
-            name: "Лук",
-            price: 0,
-            required: true,
-            additional: false
-        }, {
-            name: "Морковь",
-            price: 0,
-            required: true,
-            additional: false
-        }],
-        type: ""
-    };
-
-    const coffeeModel = new dishModel(coffee);
-    const saladModel = new dishModel(salad);
-
-    coffeeModel.save(function(err){
-        if(err) return console.log(err);
-        saladModel.save(function(err){
-            if(err) return console.log(err);
-            res.send("ok");
-        });
-    });
-});
+const orderedDishRouter = express.Router();
 
 app.get("/orderedDish", (req, res) => {
 
 });
 
+const orderRouter = express.Router();
+
 app.get("/order", (req, res) => {
 
 });
 
+const cardRouter = express.Router();
+
 app.get("/card", (req, res) => {
-    cardModel.find({}, function(err, card){
-        if(err) return console.log(err);
-        res.send(card)
+    Card.find({}, (err, card) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+        res.send(card);
     });
 });
 
+const distributionPointRouter = express.Router();
+
 app.get("/distributionPoint", (req, res) => {
-    distributionPointModel.find({}, function(err, points){
-        if(err) return console.log(err);
-        res.send(points)
+    DistributionPoint.find({}, (err, points) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+        res.send(points);
     });
 });
 
