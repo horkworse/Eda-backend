6737768feaf8ef6b574d2971c6a9e11a0f706770
horkworse <horkworse@gmail.com>
2021-11-18 14:31:18 +0500

    fix api

--- index.js
+++ index.js
@@ -29,19 +29,23 @@ const OrderStatus = mongoose.model(" orderStatus", orderStatusSchema);
 
 const connectionString = `mongodb+srv://${config.get("dbuser")}:${config.get("dbpassword")}@cluster0.phklg.mongodb.net/${config.get("dbname")}?retryWrites=true&w=majority`;
 mongoose.connect(connectionString, function (err) {
-    if (err) console.log(err);
+    if (err) log.error(err);
     app.listen(3001, () => {
-        console.log("Сервер ожидает подключения...");
+        console.log("success");
     });
 });
 
 app.use((req, res, next) => {
     log.info("request ", req.url);
+    // console.log(req.query);
+    // console.log(req.params);
+    // console.log(req.body);
+    // console.log(req.headers);
 
-    if (!req.query.token || !bcrypt.compareSync(req.query?.token, config.get("token"))) {
-        log.error("access denied");
-        return res.sendStatus(401);
-    }
+    // if (!req.query?.token || !req.body?.token || !bcrypt.compareSync(req.query?.token, config.get("token"))) {
+    //     log.error("access denied");
+    //     return res.sendStatus(401);
+    // }
     return next();
 });
 
@@ -50,55 +54,62 @@ const dishRouter = express.Router();
 dishRouter.get("/", (req, res) => {
     Dish.find({}, (err, dishes) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
-        res.send(dishes);
+        return dishes? res.send(dishes) : res.sendStatus(404);
     });
 });
 
 dishRouter.get("/:id", (req, res) => {
     Dish.findOne({_id: ObjectId(req.params.id)}, (err, dish) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
-        res.send(dish);
+        return dish? res.send(dish) : res.sendStatus(404);
     });
 });
 
 dishRouter.put("/edit", (req, res) => {
+    if(!req.body) {
+        log.error("empty body");
+        return res.sendStatus(400);
+    }
     let dish = {
-        name: req.query.name,
-        description: req.query.description,
-        price: req.query.price,
-        image: req.query.image,
-        ingredient: req.query.ingredient,
-        type: req.query.type
-    };
-
-    Dish.findOneAndUpdate({_id: req.params.id}, dish, (err, newDish) => {
-        if (err) {
-            console.log(err);
-            return res.sendStatus(400);
-        }
-        res.send(newDish.id);
-    });
-});
-
-dishRouter.post("/add", (req, res) => {
-    let dish = new Dish({
         name: req.body.name,
         description: req.body.description,
         price: req.body.price,
         image: req.body.image,
         ingredient: req.body.ingredient,
         type: req.body.type
-    });
+    };
 
+    Dish.findOneAndUpdate({_id: req.params.id}, dish, (err, newDish) => {
+        if (err) {
+            log.error(err);
+            return res.sendStatus(400);
+        }
+        res.send(newDish.id);
+    });
+});
+
+dishRouter.post("/add", jsonParser, (req, res) => {
+    if(!req.body) {
+        log.error("empty body");
+        return res.sendStatus(400);
+    }
+    let dish = new Dish({
+        name: req.query.name,
+        description: req.query.description,
+        price: req.query.price,
+        image: req.query.image,
+        ingredient: req.query.ingredient,
+        type: req.query.type
+    });
     dish.save((err, newDish) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
         res.send(newDish.id);
@@ -108,7 +119,7 @@ dishRouter.post("/add", (req, res) => {
 dishRouter.delete("/delete/:id", (req, res) => {
     Dish.findByIdAndDelete(ObjectId(req.params.id), (err) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
         res.sendStatus(200);
@@ -122,31 +133,35 @@ const dishTypeRouter = express.Router();
 dishTypeRouter.get("/", (req, res) => {
     DishType.find({}, (err, dishTypes) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
         res.send(dishTypes);
+        return dishTypes? res.send(dishTypes) : res.sendStatus(404);
     });
 });
 
 dishTypeRouter.get("/:id", (req, res) => {
     DishType.findOne({_id: ObjectId(req.params.id)}, (err, dishType) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
-        res.send(dishType);
+        return dishType? res.send(dishType) : res.sendStatus(404);
     })
 });
 
 dishTypeRouter.put("/edit", (req, res) => {
+    if(!req.body) {
+        log.error("empty body");
+        return res.sendStatus(400);
+    }
     let dishType = {
         name: req.query.name
     };
-
     DishType.findOneAndUpdate({_id: req.params.id}, dishType, (err, newDishType) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
         res.send(newDishType.id);
@@ -154,14 +169,17 @@ dishTypeRouter.put("/edit", (req, res) => {
 });
 
 dishTypeRouter.post("/add", (req, res) => {
+    if(!req.body) {
+        log.error("empty body");
+        return res.sendStatus(400);
+    }
     let dishType = new DishType({
         name: req.body.name,
         group: req.body.group
     });
-
     dishType.save((err, newDishType) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
         res.send(newDishType.id);
@@ -171,7 +189,7 @@ dishTypeRouter.post("/add", (req, res) => {
 dishTypeRouter.delete("/delete/:id", (req, res) => {
     DishType.findByIdAndDelete(ObjectId(req.params.id), (err) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
         res.sendStatus(200);
@@ -185,24 +203,29 @@ const ingredientRouter = express.Router();
 ingredientRouter.get("/", (req, res) => {
     Ingredient.find({}, (err, ingredients) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
-        res.send(ingredients);
+        return ingredients? res.send(ingredients) : res.sendStatus(404);
     });
 });
 
 ingredientRouter.get("/:id", (req, res) => {
     Ingredient.findOne({_id: ObjectId(req.params.id)}, (err, ingredient) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
-        res.send(ingredient);
+        return ingredient? res.send(ingredient) : res.sendStatus(404);
+
     });
 });
 
 ingredientRouter.put("/edit", (req, res) => {
+    if(!req.body) {
+        log.error("empty body");
+        return res.sendStatus(400);
+    }
     let newIngredient = {
         name: req.query.name,
         description: req.query.description,
@@ -211,10 +234,9 @@ ingredientRouter.put("/edit", (req, res) => {
         ingredient: req.query.ingredient,
         type: req.query.type
     };
-
     Ingredient.findOneAndUpdate({_id: req.params.id}, newIngredient, (err, ingredient) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
         res.send(ingredient.id);
@@ -222,16 +244,19 @@ ingredientRouter.put("/edit", (req, res) => {
 });
 
 ingredientRouter.post("/add", (req, res) => {
+    if(!req.body) {
+        log.error("empty body");
+        return res.sendStatus(400);
+    }
     let ingredient = new Ingredient({
         name: req.body.name,
         price: req.body.price,
         required: req.body.required,
         additional: req.body.additional
     });
-
     ingredient.save((err, newIngredient) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
         res.send(newIngredient.id);
@@ -241,7 +266,7 @@ ingredientRouter.post("/add", (req, res) => {
 ingredientRouter.delete("/delete/:id", (req, res) => {
     Ingredient.findByIdAndDelete(ObjectId(req.params.id), (err) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
         res.sendStatus(200);
@@ -255,32 +280,36 @@ const distributionPointRouter = express.Router();
 distributionPointRouter.get("/", (req, res) => {
     DistributionPoint.find({}, (err, points) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
-        res.send(points);
+        return points? res.send(points) : res.sendStatus(404);
     });
 });
 
 distributionPointRouter.get("/:id", (req, res) => {
     DistributionPoint.findOne({_id: ObjectId(req.params.id)}, (err, distributionPoint) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
         res.send(distributionPoint);
+        return distributionPoint? res.send(distributionPoint) : res.sendStatus(404);
     });
 });
 
 distributionPointRouter.put("/edit", (req, res) => {
+    if(!req.body) {
+        log.error("empty body");
+        return res.sendStatus(400);
+    }
     let distributionPoint = {
         name: req.query.name,
         address: req.query.address
     };
-
     DistributionPoint.findOneAndUpdate({_id: req.params.id}, distributionPoint, (err, newDistributionPoint) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
         res.send(newDistributionPoint.id);
@@ -288,14 +317,17 @@ distributionPointRouter.put("/edit", (req, res) => {
 });
 
 distributionPointRouter.post("/add", (req, res) => {
+    if(!req.body) {
+        log.error("empty body");
+        return res.sendStatus(400);
+    }
     let distributionPoint = new DistributionPoint({
         name: req.query.name,
         address: req.query.address
     });
-
     distributionPoint.save((err, newDistributionPoint) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
         res.send(newDistributionPoint.id);
@@ -305,7 +337,7 @@ distributionPointRouter.post("/add", (req, res) => {
 distributionPointRouter.delete("/delete/:id", (req, res) => {
     DistributionPoint.findByIdAndDelete(ObjectId(req.params.id), (err) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
         res.sendStatus(200);
@@ -313,8 +345,12 @@ distributionPointRouter.delete("/delete/:id", (req, res) => {
 });
 
 app.use("/distributionPoint", distributionPointRouter);
+//to-do
+// 1) доделать остальные по примерам сверху
+// 2) протестировать все запросы
+// 3) валидация и ПК зренмк
+// 4)
 
-/////////////////////////////////////////
 
 const userRouter = express.Router();
 
@@ -343,7 +379,7 @@ const cardRouter = express.Router();
 app.get("/card", (req, res) => {
     Card.find({}, (err, card) => {
         if (err) {
-            console.log(err);
+            log.error(err);
             return res.sendStatus(400);
         }
         res.send(card);
